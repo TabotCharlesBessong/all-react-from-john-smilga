d05b49ac2995ada12bb103b724b4591d8e55e70f
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-08 08:39:23 +0100

    lets learn axios

---
+++ 24-axios/starter/src/final/axios/interceptors.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const authFetch = axios.create({
+  baseURL: 'https://course-api.com',
+});
+
+authFetch.interceptors.request.use(
+  (request) => {
+    request.headers.common['Accept'] = 'application/json';
+    console.log('request sent');
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(
+  (response) => {
+    console.log('got response');
+    return response;
+  },
+  (error) => {
+    console.log(error.response);
+    if (error.response.status === 404) {
+      // do something
+      console.log('NOT FOUND');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default authFetch;
