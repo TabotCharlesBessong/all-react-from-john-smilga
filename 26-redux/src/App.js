8edf25f8b2d142f647c426ce23fed26dad1bdff3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-16 15:43:04 +0100

    basic redux

--- 26-redux/src/App.js
+++ 26-redux/src/App.js
@@ -5,13 +5,46 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import {createStore} from 'redux'
+import { DECREASE, INCREASE } from "./actions";
+
+// initial store
+const initialState = {
+  count:3 ,
+  name:"John"
+}
+
+
+
+//reducer
+const reducer = (state,action)=>{
+  console.log({state,action});
+  if(action.type === DECREASE ){
+    return {...state, count:state.count - 1 }
+  }
+  if(action.type === INCREASE ){
+    return {...state, count:state.count + 1 }
+  }
+  else{
+    return state
+  }
+  // return state
+}
+
+const store = createStore(reducer,initialState)
+store.dispatch({type:DECREASE})
+store.dispatch({type:INCREASE})
+store.dispatch({type:INCREASE})
+store.dispatch({type:INCREASE})
+
+console.log(store.getState());
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart = {store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
---
+++ 26-redux/src/actions.js
@@ -0,0 +1,7 @@
+
+export const DECREASE = 'DECREASE'
+export const INCREASE = 'INCREASE'
+export const REMOVE = 'REMOVE'
+export const GET_TOTAL = 'GET_TOTAL'
+export const CLEAR_CART = 'CLEAR_CART'
+export const GET_AMOUNT = 'GET_AMOUNT'
