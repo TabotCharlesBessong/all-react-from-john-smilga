0129d78a2303687d1d4b97b525d92d3de15ddbfe
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-22 07:58:16 +0100

    lets take a break

--- 02-tours/TSTours/src/App.tsx
+++ 02-tours/TSTours/src/App.tsx
@@ -1,14 +1,33 @@
 import React,{useState} from 'react'
 import {Wrapper,Button} from './App.styles'
+import { useQuery } from 'react-query';
+import { LinearProgress, Grid } from '@material-ui/core';
 
-export type BirthdayType = {
+export type TourType = {
   id: number;
-  name: string;
-  age:number;
   image: string;
+  info:string;
+  price:number;
+  name: string;
 };
 
+const getTours = async (): Promise<TourType> => await(await fetch('https://course-api.com/react-tours-project')).json()
+
 const App = () => {
+  const [loading,setLoading] = useState(true)
+  const [tours,setTours] = useState([] as TourType[])
+  const {data,isLoading,error} = useQuery<TourType[]>(
+    'tours',
+    getTours
+  )
+  console.log(data)
+
+  const removeTours = (id:number)=>{
+    const newTours = tours.filter((tour)=>
+      tour.id !== id
+    )
+    setTours(newTours)
+  }
   return (
     <Wrapper>
       <h1>Hello my world</h1>
